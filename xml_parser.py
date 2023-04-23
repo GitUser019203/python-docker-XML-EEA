7f24442d7c842956857df7469f5692c61cc19a2a
GitUser019203 <mathkuzh@yahoo.com>
2023-04-23 12:17:57 -0400

    Changed the maximum size of the XML allowed and multihreading.

--- xml_parser.py
+++ xml_parser.py
@@ -1,20 +1,18 @@
 from sys import getsizeof
 from threading import Thread
-from helpers import print_RAM_usage, parse_XML
+from helpers import parse_XML, print_VM_usage
 
-xml_string = input("Enter an XML string at most 1 KB large: \n")
+xml_string = input("Enter an XML string at most 10 KB large: \n")
 xml_size = getsizeof(xml_string)
 
-if xml_size > 1024:
-    print(f"The XML string you entered is larger than 1 KB because it is {xml_size} KB large.")
+if xml_size > 10240:
+    print(f"The XML string you entered is larger than 10 KB because it is {xml_size} KB large.")
     exit(1)
 else:
-    print("The XML string you entered is at most 1 KB large.")
+    print("The XML string you entered is at most 10 KB large.")
 
-ram_usage_thread = Thread(target = print_RAM_usage)
-xml_parsing_thread = Thread(target = parse_XML, args = (xml_string,))
-xml_parsing_thread.start()
-ram_usage_thread.start()
-xml_parsing_thread.join()
-ram_usage_thread.join()
+vm_usage_thread = Thread(target = print_VM_usage)
+parse_XML(xml_string)
+vm_usage_thread.start()
+vm_usage_thread.join()
 
