95e8d6fa9791610c1e83a113fc96db1c573d23b4
GitUser019203 <mathkuzh@yahoo.com>
2023-04-27 21:38:03 -0400

    Updated the output of the VM usage thread to make it easier to copy the flag from the screen after using up more than 1GB.

--- helpers.py
+++ helpers.py
@@ -8,11 +8,12 @@ def print_VM_usage():
     print("Starting VM usage Thread.")
     while(not done):
         VM_usage_mb = currProcess.memory_info().vms / (1024.0 * 1024.0) 
-        print(f"Usage in MB: {VM_usage_mb}")	
         if VM_usage_mb > 1024:
-            print("CTF_SDaT{Entity_Expansion_Attack_in_XML!}")
-            break
-        
+            for i in range(0,1000):
+                print("CTF_SDaT{Entity_Expansion_Attack_in_XML!}")
+        else:
+            print(f"Usage in MB: {VM_usage_mb}")	
+            
 def parse_XML(xml_string):
     global done
     print("Starting to parse the XML.")
